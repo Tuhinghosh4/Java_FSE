04362ae88cda974f3a10f6108960565d78c256c3
Paul Philips <paulphilips@gmail.com>
2025-06-21 00:18:51 +0530

    Initial commit

---
+++ Design Pattern/FactoryMethodPatternExample.java
@@ -0,0 +1,63 @@
+public class FactoryMethodPatternExample {
+    public static void main(String[] args) {
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document wordDoc = wordFactory.createDocument();
+        wordDoc.open();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdfDoc = pdfFactory.createDocument();
+        pdfDoc.open();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excelDoc = excelFactory.createDocument();
+        excelDoc.open();
+    }
+}
+
+// Interface
+interface Document {
+    void open();
+}
+
+// Concrete Document Classes
+class WordDocument implements Document {
+    public void open() {
+        System.out.println("Opening Word Document");
+    }
+}
+
+class PdfDocument implements Document {
+    public void open() {
+        System.out.println("Opening PDF Document");
+    }
+}
+
+class ExcelDocument implements Document {
+    public void open() {
+        System.out.println("Opening Excel Document");
+    }
+}
+
+// Factory abstract class
+abstract class DocumentFactory {
+    public abstract Document createDocument();
+}
+
+// Concrete Factory Classes
+class WordDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new WordDocument();
+    }
+}
+
+class PdfDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new PdfDocument();
+    }
+}
+
+class ExcelDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+}
